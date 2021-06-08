ee2aa0da14277bd63004e045493dc03a9d56808e
sonya-sibagatova <sibsonya@mail.com>
2021-06-08 14:41:53 +0300

    filter_peaks.R

---
+++ src/filter_peaks.R
@@ -0,0 +1,47 @@
+setwd("/home/sonya/Downloads/minor_project/")
+
+# install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
+
+###
+
+NAME <- 'H3K27me3_MEL.ENCFF684CIQ.mm10'
+# 1500 for ENCFF684CIQ
+# NAME <- 'H3K27me3_MEL.ENCFF932OHQ.mm10'
+# 600 for ENCFF932OHQ, 
+
+DATA_DIR <- "/home/sonya/Downloads/minor_project/"
+OUT_DIR <- "/home/sonya/Downloads/minor_project/images"
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
+
+# Remove long peaks
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 1500)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
+
+bed_df %>%
+  select(-len) %>%
+  write.table(file=paste0(DATA_DIR, NAME ,'.filtered.bed'),
+              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+
